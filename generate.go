51df98d9071c856c226e7863a7704c684b24dbb4
Thomas De Meyer <t.demeyer@labdigital.nl>
2024-08-23 14:09:12 +0200

    feat: added global settings sdk

--- generate.go
+++ generate.go
@@ -1,4 +1,5 @@
 package bluestonepim_go_sdk
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config pim/config.yaml https://docs.api.bluestonepim.com/openapi/66557c17ba1fbb0011cf1af6
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config notification_external/config.yaml https://mapi.bluestonepim.com/notification-external/v2/api-docs
+//go:generate oapi-codegen -config pim/config.yaml https://docs.api.bluestonepim.com/openapi/66557c17ba1fbb0011cf1af6
+//go:generate oapi-codegen -config notification_external/config.yaml https://mapi.bluestonepim.com/notification-external/v2/api-docs
+//go:generate oapi-codegen -config global_settings/config.yaml https://docs.api.bluestonepim.com/openapi/66558009ac2b390018a0bcb6
